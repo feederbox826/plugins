495fe32f06db059453ac23bfa387ae84920067e1
feederbox826 <feederbox826@users.noreply.github.com>
2024-01-13 02:30:18 -0500

    clean wfke, remove console log, clean properties

--- plugins/tag-video/tag-video.js
+++ plugins/tag-video/tag-video.js
@@ -1,33 +1,32 @@
 (function () {
+    'use strict'
     // wait for visible key elements and return
     function wfke(selector, callback) {
         var el = document.querySelector(selector)
-        if (el) return callback(el)
+        if (el) return callback()
         setTimeout(wfke, 100, selector, callback)
     }
-    'use strict'
 
     const tagImgSelector = ".detail-header-image img, .tag-card-header img"
     const bgImgSelector = ".background-image-container img"
 
-    const waitImg = () => wfke(tagImgSelector, replaceAll)
-    const replaceAll = () => document.querySelectorAll(`${tagImgSelector}, ${bgImgSelector}`).forEach(replace)
+    const replaceAll = () => wfke(tagImgSelector, () =>
+        document.querySelectorAll(`${tagImgSelector}, ${bgImgSelector}`)
+            .forEach(replace)
+    )
 
     function replace(img) {
-        console.log("replacing")
         const src = img.getAttribute("src")
         const video = document.createElement("video")
+        const propName = ["autoplay", "muted", "loop", "playsinline"]
+        propName.forEach(prop => video[prop] = true)
         video.attributes.alt = img.attributes.alt
         video.classList = img.classList
-        video.autoplay = true
-        video.muted = true
-        video.loop = true
         video.src = src
         video.poster = src
-        video.setAttribute("playsinline", true)
         img.replaceWith(video)
     }
-    stash.addEventListener('page:tag', waitImg)
-    stash.addEventListener('page:tag:any', waitImg)
-    stash.addEventListener('page:tags', waitImg)
+    stash.addEventListener('page:tag', replaceAll)
+    stash.addEventListener('page:tag:any', replaceAll)
+    stash.addEventListener('page:tags', replaceAll)
 })()
