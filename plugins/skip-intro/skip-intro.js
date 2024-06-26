754d666b92e40080326aea83f7e0dee597cd4942
feederbox826 <feederbox826@users.noreply.github.com>
2024-06-26 19:50:20 -0400

    add networks upport to skip-intro

--- plugins/skip-intro/skip-intro.js
+++ plugins/skip-intro/skip-intro.js
@@ -1,5 +1,9 @@
 // handle entries from remote
-const introMap = new Map(Object.entries(intros));
+// import cache from localStorage
+const introMap = new Map(Object.entries({
+  ...intros,
+  ...JSON.parse(localStorage.getItem("skip-intro-cache") || "{}")
+}))
 
 // hook into video element for skipping
 const hookVideo = (endTime) => wfke("video-js>video", () => {
@@ -16,10 +20,37 @@ const hookVideo = (endTime) => wfke("video-js>video", () => {
   // only fire events once per video
 });
 
+async function cacheNetwork() {
+  // cache network sub-studios
+  // check if cache is up-to-date
+  const cache = localStorage.getItem("skip-intro-cache-len");
+  if (cache && cache == networks.length) return;
+  // fetch from StashDB
+  // add timeout for stashdb
+  if (!window?.stashdb) return setTimeout(cacheNetwork, 500)
+  const networkCache = {};
+  for (const network of Object.keys(networks)) {
+    const introTime = networks[network];
+    const query = `query ($id: ID) {
+      findStudio(id: $id) {
+      child_studios {
+          id
+      }}}`
+    const variables = { id: network };
+    const dbResponse = await stashdb.callGQL({ query, variables })
+    dbResponse.findStudio.child_studios
+      .map(studio => studio.id)
+      .forEach(studio => networkCache[studio] = introTime);
+    // update localstorage cache
+    localStorage.setItem("skip-intro-cache-len", networks.length);
+    localStorage.setItem("skip-intro-cache", JSON.stringify(networkCache));
+  }
+}
+
 // ready on page reloads
 function readyPage(event) {
   // intercept GQL request
-  if (!event.detail.data.findScene) return; // only trigger on findScene
+  if (!event.detail?.data?.findScene) return; // only trigger on findScene
   // get stashDB studioID
   const studioID = event.detail.data.findScene.studio.stash_ids.find(
     (id) => id.endpoint == "https://stashdb.org/graphql",
@@ -31,3 +62,4 @@ function readyPage(event) {
   }
 }
 fbox826.gqlListener.addEventListener("response", readyPage);
+cacheNetwork()
