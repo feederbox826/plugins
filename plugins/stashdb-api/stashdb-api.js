582706d778a3bd198196146fca389906c3f4d59b
feederbox826 <feederbox826@users.noreply.github.com>
2024-06-26 19:50:06 -0400

    faster stashdbapi-setup

--- plugins/stashdb-api/stashdb-api.js
+++ plugins/stashdb-api/stashdb-api.js
@@ -1,13 +1,15 @@
-(async function () {
+async function setupStashDB() {
     // get apikey from Configuration
     const apikey_query = `query { configuration { general {
     stashBoxes {
         endpoint
         api_key
     }}}}`;
-    const response = await csLib.callGQL({ query: apikey_query });
-    const apikey = response.configuration.general.stashBoxes
-      .find(stashbox => stashbox.endpoint === "https://stashdb.org/graphql").api_key
+
+    const apikey = await csLib.callGQL({ query: apikey_query })
+      .then(res => res.configuration.general.stashBoxes
+        .find(stashbox => stashbox.endpoint === "https://stashdb.org/graphql").api_key
+      )
 
     const callGQL = (reqData) =>
       fetch(`https://stashdb.org/graphql`, {
@@ -25,4 +27,5 @@
     window.stashdb = {  
       callGQL,
     };
-  })();
+}
+setupStashDB();
