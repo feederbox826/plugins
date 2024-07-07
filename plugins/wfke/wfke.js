77d49826f926912a53e7a80fc5426a74bce7a2e0
feederbox826 <feederbox826@users.noreply.github.com>
2024-07-06 22:02:07 -0400

    [wfke] add element callback

--- plugins/wfke/wfke.js
+++ plugins/wfke/wfke.js
@@ -1,6 +1,6 @@
 // wait for visible key elements
 function wfke(selector, callback) {
     var el = document.querySelector(selector);
-    if (el) return callback();
+    if (el) return callback(el);
     setTimeout(wfke, 100, selector, callback);
 }
