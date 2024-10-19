908d39d8cafe77053412a38b3c9e32598ed1a8b3
feederbox826 <feederbox826@users.noreply.github.com>
2024-10-18 20:22:27 -0400

    [studio-img-bg] hunt down svgs not returning height,width

--- plugins/studio-img-bg/studio-img-bg.js
+++ plugins/studio-img-bg/studio-img-bg.js
@@ -2,17 +2,19 @@ function addStudioImgBG() {
     // image dark/light detector from
     // https://stackoverflow.com/a/38213966
     // modified to exclude alpha pixels
-    function queryBrightness(img) {
+    function queryBrightness(dims, img) {
         let colorSum = 0;
         let alphaPixels = 0;
         const canvas = document.createElement("canvas");
-        canvas.width = img.width;
-        canvas.height = img.height;
+        const width = dims[0]
+        const height = dims[1]
+        canvas.width = width;
+        canvas.height = height;
 
         const ctx = canvas.getContext("2d");
         ctx.drawImage(img,0,0);
 
-        const data = ctx.getImageData(0,0,canvas.width,canvas.height).data
+        const data = ctx.getImageData(0,0,width,height).data
         for(let x = 0, len = data.length; x < len; x+=4) {
             if (data[x+3] === 0) {
                 alphaPixels++
@@ -20,12 +22,33 @@ function addStudioImgBG() {
             }
             colorSum += Math.floor((data[x] + data[x+1] + data[x+2])/3); // average value of R, G and B
         }
-        const alphaRatio = alphaPixels / (img.width*img.height)
-        return [Math.floor(colorSum / ((img.width*img.height)-alphaPixels)), alphaRatio]
+        const alphaRatio = alphaPixels / (width*height)
+        return [Math.floor(colorSum / ((width*height)-alphaPixels)), alphaRatio]
     }
     const selector = ".studio-overlay img.image-thumbnail:not(.checked), img.studio-logo:not(.checked), .studio-card-header img.studio-card-image:not(.checked)"
-    const checkImage = (lookup, target) => {
-        const [brightness, alphaRatio] = queryBrightness(lookup)
+    const targetDimensions = async (target) => {
+        const height = target.height || target.clientHeight
+        const width = target.width || target.clientWidth
+        if (height && width) return [height, width]
+        else if (!height && !width) {
+            // inject svg
+            const dims = await fetch(target.src)
+                .then(res => res.text())
+                .then(data => {
+                    const ct = document.createElement("svg")
+                    ct.innerHTML = data
+                    const vbox = ct.children[0].getAttribute("viewBox")
+                    delete ct
+                    return vbox
+                })
+            const [,,width, height] = dims.split(" ")
+            return [width, height]
+        }
+    }
+    const checkImage = async (lookup, target) => {
+        // get dimensions
+        const dims = await targetDimensions(lookup)
+        const [brightness, alphaRatio] = queryBrightness(dims, target)
         target.classList.add("checked")
         target.classList.add(
             brightness >= 155 ? "light" // if over 155, it's light
