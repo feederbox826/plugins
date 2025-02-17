6a25caadcf6ff9eb2a0875b4eb2dac5e0d0de3bf
feederbox826 <feederbox826@users.noreply.github.com>
2025-02-17 02:53:56 -0500

    debug, not trace

--- plugins/cuptag/cuptag.js
+++ plugins/cuptag/cuptag.js
@@ -132,7 +132,7 @@ function setPerformer(id, measurements) {
     const cupRegex = /\d{2}([A-H]{1}|A{1,3}|D{2,4})(?:-\d{2}-\d{2})?/
     if (!cupRegex.test(measurements)) {
         log.Debug("No eligible cup size found")
-        log.Trace(measurements)
+        log.Debug(measurements)
         return
     }
     let cupSize = measurements.match(cupRegex)[1]
