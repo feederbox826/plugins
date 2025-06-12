05de81b4856644584b940cfb3727b06f44a8e799
feederbox826 <feederbox826@users.noreply.github.com>
2025-06-12 02:45:26 -0400

    [stashdb-api] consistent spacing, minify code

--- plugins/stashdb-api/stashdb-api.js
+++ plugins/stashdb-api/stashdb-api.js
@@ -1,31 +1,30 @@
 async function setupStashDB() {
-    // get apikey from Configuration
-    const apikey_query = `query { configuration { general {
-    stashBoxes {
-        endpoint
-        api_key
-    }}}}`;
+  // get apikey from Configuration
+  const apikeyQuery = `query { configuration { general {
+  stashBoxes {
+    endpoint
+    api_key
+  }}}}`;
 
-    const apikey = await csLib.callGQL({ query: apikey_query })
-      .then(res => res.configuration.general.stashBoxes
-        .find(stashbox => stashbox.endpoint === "https://stashdb.org/graphql").api_key
-      )
+  const apikey = await csLib.callGQL({ query: apikeyQuery })
+    .then(res => res.configuration.general.stashBoxes
+      .find(stashbox => stashbox.endpoint === "https://stashdb.org/graphql").api_key
+    );
 
-    const callGQL = (reqData) =>
-      fetch(`https://stashdb.org/graphql`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "ApiKey": apikey,
-        },
-        body: JSON.stringify(reqData),
-      })
-        .then((res) => res.json())
-        .then((res) => res.data);
+  const callGQL = (reqData) =>
+    fetch(`https://stashdb.org/graphql`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "ApiKey": apikey
+      },
+      body: JSON.stringify(reqData)
+    }).then(res => res.json())
+      .then(res => res.data);
 
-    // export to window
-    window.stashdb = {  
-      callGQL,
-    };
+  // export to window
+  window.stashdb = {
+    callGQL
+  };
 }
 setupStashDB();
