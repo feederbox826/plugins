1cb80737668b33ba4b4e9bd64d66e62dd4f87624
feederbox826 <feederbox826@users.noreply.github.com>
2026-03-29 21:57:59 -0400

    [skip-intro] update for skips-db integration

--- plugins/skip-intro/README.md
+++ plugins/skip-intro/README.md
@@ -9,12 +9,15 @@ Reads from the custom_field `skip_first` which is a number for how many seconds 
 1. Scene
 2. Studio
 3. Parent of Studio
+4. (Remote) Studio
+5. (Remote) Parent of Studio
 
-If there is no `skip_first` at a lower level, it will default to the higher level until a value is found. If no value is found, there will be no auto-skip.
+If there is no `skip_first` at a lower level, it will default to the higher level until a value is found.
+If no results are found locally, skips-db is queried at the studio and parent studio level.
 
 After pausing, you can use the following command to get the time to skip to.
 ```js
 document.querySelector("video-js>video").currentTime
 ```
 
-Pulling from the [database](https://github.com/feederbox826/stash-skip-intro) has been removed. Everything now stays local to your stash, the old times have not been imported
+The static [stash-skip-intro](https://github.com/feederbox826/stash-skip-intro) database has been replaced with [skips-db](https://github.com/feederbox826/skips-db) - a crowdsourced instance for skip times. All existing times have been imported. Local times (including 0 for no intro/ inconsistent intro) can be submitted via [skip-intro-sync](https://github.com/feederbox826/plugins/tree/main/plugins/skip-intro-sync)
--- plugins/skip-intro/skip-intro.js
+++ plugins/skip-intro/skip-intro.js
@@ -12,19 +12,42 @@ const hookVideo = (endTime) => wfke("video-js>video", (video) => {
   // only fire events once per video
 });
 
+// utils
+const get_skip_first = (obj) => obj?.custom_fields?.["skip_first"]; // for consistent custom_field name
+const get_stashdb_id = (stashids) => stashids?.find(id => id.endpoint === "https://stashdb.org/graphql")?.stash_id
+
 // get skip_first custom field
 const getStudioSkip = id => csLib.callGQL({
   query: `query ($id: ID!) {
   findStudio(id: $id) {
-    custom_fields parent_studio { custom_fields }
+    custom_fields
+    stash_ids { stash_id endpoint }
+    parent_studio {
+      custom_fields
+      stash_ids { stash_id endpoint }
+    }
   }}`,
   variables: { id }
 }).then(data =>
-  // get skip_first custom field from scene, studio or parent studio
-  data.findStudio?.custom_fields?.["skip_first"]
-    ?? data.findStudio?.parent_studio?.custom_fields?.["skip_first"]
-    ?? null
+  //get skip_first custom field from scene, studio or parent studio
+  get_skip_first(data.findStudio)
+    ?? get_skip_first(data.findStudio?.parent_studio)
+    ?? getRemoteStudioSkip(data.findStudio.stash_ids, data.findStudio.parent_studio?.stash_ids)
 )
+
+const getRemoteSkipTime = async (id) => fetch(`https://skips.feederbox.cc/api/time/${id}`)
+  .then(res => res.json())
+  .then(data => data.skip_seconds)
+  .catch(() => undefined) // if error/ 404, return undefined
+
+const getRemoteStudioSkip = async (stashids, parent_stashids) => {
+  const stashid = get_stashdb_id(stashids);
+  const parent_stashid = get_stashdb_id(parent_stashids);
+  console.log("[skip-intro] fetching from remote", { stashid, parent_stashid });
+  return stashid ? await getRemoteSkipTime(stashid)
+    : parent_stashid ? await getRemoteSkipTime(parent_stashid)
+      : null
+}
 
 // ready on page reloads
 async function readyPage(event) {
@@ -33,7 +56,7 @@ async function readyPage(event) {
   if (!findScene) return; // only trigger on findScene
   // check if custom skip_first in scene custom fields
   // otherwise check studio and parent studio custom fields for skip_first
-  const skip_time = findScene.custom_fields?.["skip_first"]
+  const skip_time = get_skip_first(findScene)
     ?? await getStudioSkip(findScene.studio.id)
   // try studio first
   if (skip_time) {
